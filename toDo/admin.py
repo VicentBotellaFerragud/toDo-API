19be8630773762e9ffb2a731ddbe1b3b838c45bf
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-06-27 21:47:54 +0200

    superuser created, admin.py file updated

--- toDo/admin.py
+++ toDo/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import toDo
 
+class toDoAdmin(admin.ModelAdmin):    
+    fields = ('title', 'description', 'created_at', 'user',)    
+    list_display = ('title', 'description', 'created_at', 'user',)    
+    search_fields = ('title',)
+
 # Register your models here.
 
-admin.site.register(toDo)
+admin.site.register(toDo, toDoAdmin)
