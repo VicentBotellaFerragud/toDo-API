9a5635c1621cca447e6ff2d3bfec1d938711c0a7
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-07-03 20:36:58 +0200

    create() function created (function that's always called by POST request)

--- toDo/views.py
+++ toDo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import toDo
 from .serializers import toDoSerializer
+from django.core import serializers
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -12,3 +14,11 @@ class toDoViewSet(viewsets.ModelViewSet):
     queryset = toDo.objects.all().order_by('created_at')
     serializer_class = toDoSerializer
     permission_classes = [] # permissions.IsAuthenticated --> It requires you to be logged in to use the API.
+
+    def create(self, request): # This function is always called by POST requests.
+        newToDo = toDo.objects.create(title = request.POST.get('title', ''), 
+                                      description = request.POST.get('description', ''), 
+                                      user = request.user,)
+
+        serialized_toDo = serializers.serialize('json', [newToDo, ]) 
+        return HttpResponse(serialized_toDo, content_type ='application/json')
